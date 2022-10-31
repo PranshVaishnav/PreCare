965479be2c17ee7bdf24d8490cdcd1342a5da694
Priyanshu <princepriyanshu.vaishnav@gmail.com>
2022-10-31 14:31:46 +0530

    Set up flask and mern stack

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+
+import "./index.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("/members")
+            .then(
+                (res) => res.json()
+            )
+            .then(
+                (res) => {
+                    setData(res.members)
+                })
+    }, []);
+
+    return (
+
+        <div>
+            <p>Hello</p>
+
+            {
+                data.map((val) => {
+                    return (<p>{val}</p>)
+                })
+            }
+
+        </div>
+    )
 }
 
-export default App;
+export default App
